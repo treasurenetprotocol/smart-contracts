65e0ee128403a222495fc72b8e47f49a56c43470
chenhuiyuan <18710160187@163.com>
2025-02-12 15:24:21 +0800

    初始化

--- contracts/Governance/IRoles.sol
+++ contracts/Governance/IRoles.sol
@@ -1,9 +1,12 @@
 // SPDX-License-Identifier: GPL-3.0
 pragma solidity ^0.8.0;
-import "@openzeppelin/contracts/access/IAccessControlEnumerable.sol";
 
-interface IRoles is IAccessControlEnumerable {
-    // Role definitions
-    function CROSSCHAIN_SENDER() external pure returns (bytes32);
+import "@openzeppelin/contracts-upgradeable/access/IAccessControlUpgradeable.sol";
 
+/**
+ * @title IRoles
+ * @dev 自定义接口，可在此扩展额外的角色相关方法，目前直接继承 upgradeable 的 IAccessControl 接口
+ */
+interface IRoles is IAccessControlUpgradeable {
+    // 如果需要添加额外函数，可以在此处定义
 }
