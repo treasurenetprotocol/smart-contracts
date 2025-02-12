67d087db07e82d1a667b850fca0509c7a4329f40
chenhuiyuan <18710160187@163.com>
2025-02-12 16:04:10 +0800

    初始化

--- migrations/2.airdrop.js
+++ migrations/2.airdrop.js
@@ -10,5 +10,5 @@ const AirDrop = artifacts.require("AirDrop");
 const { deployProxy } = require("@openzeppelin/truffle-upgrades");
 
 module.exports = async function (deployer, network, accounts) {
-    await deployProxy(AirDrop, { initializer: false }, { deployer });
+    // await deployProxy(AirDrop, { initializer: false }, { deployer });
 };
