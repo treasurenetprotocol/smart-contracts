1473d9939f6766649451939f9771def3ec1742ec
chriszhao1988 <chriszhao1988@hotmail.com>
2025-04-29 20:21:58 +0800

    test

--- truffle-config.js
+++ truffle-config.js
@@ -43,8 +43,8 @@ module.exports = {
       provider: () =>
         new HDWalletProvider({
           privateKeys: privateKeysMainnet,
-          providerOrUrl: "https://mainnet.infura.io/v3/b48b6730598247c889a8dd841d941e3b",
-          pollingInterval: 30000,
+          providerOrUrl: "https://fluent-sparkling-brook.quiknode.pro/92c65096cefab8227cb8801c3226778e73fdd03f/",
+          pollingInterval: 1800000,
           networkCheckTimeout: 1000000000,
           timeoutBlocks: 200000,
         }),
